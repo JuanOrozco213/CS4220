01496bc928391f99e6d6f2e3d883f2e8f7895fc1
cydneymikel <cydney.m.auman@jpl.nasa.gov>
2018-03-04 14:03:00 -0800

    week 6.

---
+++ Week06/examples/args.js
@@ -0,0 +1,17 @@
+// The first element will be the Node.js executable path.
+// The second element will be the path to the.js file being executed.
+// The remaining elements will be any additional command line arguments.
+const args = process.argv
+console.log(args)
+
+
+
+
+
+
+
+
+
+// Syntax for executing CLI
+// node file.j --help
+// node file.js --operators + - * / --min 0 --max 10
---
+++ Week06/examples/cards-app/cli.js
@@ -0,0 +1,4 @@
+const
+    app = require('./app'),
+    yargs = require('yargs')
+
